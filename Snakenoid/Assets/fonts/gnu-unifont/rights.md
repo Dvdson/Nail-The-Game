90d21f03fc35161b0495d4a88e4bfc7cf56a2172
dvdson <dvdsoncomp@gmail.com>
2019-08-03 15:33:46 -0300

    Criado main menu e selecionador.
    Um prototipo de menu inicial foi criado, seguidamente de um elemento de
    seleÃÃo para indicar ao usuÃrio a opÃÃo do menu.

---
+++ Snakenoid/Assets/fonts/gnu-unifont/rights.md
@@ -0,0 +1,19 @@
+- Category
+
+  [Monospaced](https://fontlibrary.org/en/search?category=Monospaced)
+
+- License
+
+  [GNU General Public License](http://www.gnu.org/copyleft/gpl.html)
+
+- Family
+
+  [GNU Unifont](https://fontlibrary.org/en/font/gnu-unifont)
+
+- Description
+
+  *Free and open-source bitmap font by Roman Czyborra and Paul Hardy*
+
+- Link
+
+  <https://fontlibrary.org/en/font/gnu-unifont>
---
+++ Snakenoid/Main/Scripts/Control.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var selected = 0
+onready var sel_list = [$"select_start", $"select_credits"]
+
+func set_selected(val):
+	for index in range(sel_list.size()):
+		if val == index:
+			sel_list[index].set_visible(true)
+			selected = val
+		else:
+			sel_list[index].set_visible(false)
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		if(selected > 0):
+			set_selected(selected - 1)
+	if Input.is_action_pressed("ui_down"):
+		if(selected < sel_list.size() - 1):
+			set_selected(selected+1)
+
+func _ready():
+	set_selected(0)
